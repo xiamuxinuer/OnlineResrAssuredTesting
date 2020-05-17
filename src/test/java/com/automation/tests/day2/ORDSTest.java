6494f96b94bc987a87207af4a4efd54ca8fb005a
xiamuxinuer <emmaxiaogaiti@gmail.com>
2020-05-17 16:01:51 -0500

    test created

--- src/test/java/com/automation/tests/day2/ORDSTest.java
+++ src/test/java/com/automation/tests/day2/ORDSTest.java
@@ -1,6 +1,7 @@
 package com.automation.tests.day2;
 
 import io.restassured.response.Response;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -25,8 +26,21 @@ Response response= given().baseUri(base_URL).when().get("/employees").prettyPeek
 @DisplayName("get employee under specific id")
 public void getOneEmployee(){
     Response response= given().baseUri(base_URL).when().get("/employees/{id}",100).prettyPeek();
+
+    int statusCode = response.statusCode();// to get status code
+    System.out.println(statusCode);    //to print status code
+    Assertions.assertEquals(200,statusCode);
+
+    //assertion:
+    response.then().statusCode(200);
+
 }
 
+@Test
+    @DisplayName("get countries info")
+    public void getCountriesInfo(){
+   given().baseUri(base_URL).when().get("/countries").prettyPeek().then().statusCode(200);
+}
 
 
 
---
+++ src/test/java/com/automation/tests/day2/SpartansTest.java
@@ -0,0 +1,45 @@
+package com.automation.tests.day2;
+
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+
+import static io.restassured.RestAssured.*;
+
+public class SpartansTest {
+
+    String base_URL="http://3.90.112.152:8000";
+
+    @Test
+    @DisplayName("get list of all spartans")
+    public void getAllSpartans(){
+        given().auth().basic("admin","admin").
+                baseUri(base_URL).when().get("/api/spartans").prettyPeek().then().statusCode(200);
+
+    }
+
+    @Test
+    @DisplayName("add new spartan")
+    public void addNewSpartan(){
+
+//String body= "{\"gender\": \"Male\", \"name\": \"Random User\", \"phone\": 1059999999}";
+        File jsonFile = new File(System.getProperty("user.dir") + "/spartan.json");
+
+        given().contentType(ContentType.JSON).
+                auth().basic("admin","admin").body(jsonFile).
+                baseUri(base_URL).post("/api/spartans").prettyPeek().then().statusCode(201);
+
+
+    }
+
+
+
+
+
+
+
+
+
+}
